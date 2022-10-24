85c2f9eed4e4f2ca9cc89755089e3a1dc5fd60f0
maxefimov <efimov.top.academy@gmail.com>
2022-10-24 21:25:01 +0300

    24-10-22

--- NetClientSocketProject/Program.cs
+++ NetClientSocketProject/Program.cs
@@ -7,33 +7,52 @@ namespace NetClientSocketProject
 {
     internal class Program
     {
+
         static int port = 10001;
 
         static string addressServer = "127.0.0.1";
-        static void Main(string[] args)
+
+        static void SendMessageToServer()
         {
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(addressServer), port);
             Socket socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             socketClient.Connect(endPoint);
-            string str = "Client message to server";
-            byte[] buffer = Encoding.Default.GetBytes(str);
 
-            socketClient.Send(buffer);
-
-            buffer = new byte[1024];
-            StringBuilder strServer = new();
-
-            do
+            while (true)
             {
-                int byteCount = socketClient.Receive(buffer, buffer.Length, SocketFlags.None);
-                strServer.Append(Encoding.Default.GetString(buffer, 0, byteCount));
-            } while (socketClient.Available > 0);
+                string str;
+                Console.Write($"Input message: ");
+                str = Console.ReadLine();
 
-            Console.WriteLine($"Server answer: {strServer.ToString()}");
+                byte[] buffer = Encoding.Default.GetBytes(str);
+
+                socketClient.Send(buffer);
+
+                buffer = new byte[1024];
+                StringBuilder strServer = new();
+
+                do
+                {
+                    int byteCount = socketClient.Receive(buffer, buffer.Length, SocketFlags.None);
+                    strServer.Append(Encoding.Default.GetString(buffer, 0, byteCount));
+                } while (socketClient.Available > 0);
+
+                Console.WriteLine($"{DateTime.Now.ToShortDateString()} Server answer: {strServer.ToString()}");
+
+                if (str.IndexOf("quit") != -1)
+                    break;
+                    
+            }
+            
+            //string str = "Client message to server";
 
             socketClient.Shutdown(SocketShutdown.Both);
             socketClient.Close();
+        }
+        static void Main(string[] args)
+        {
+            SendMessageToServer();
 
             Console.ReadKey();
         }
